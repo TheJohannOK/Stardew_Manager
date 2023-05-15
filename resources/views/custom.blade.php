19aaa01b9852e0aef9a8f600a3bd9851a2eb967a
johann <johann.teran2000@gmail.com>
2023-05-16 00:04:58 +0200

    Segundo_commit

---
+++ resources/views/custom.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Custom</title>
+</head>
+<body>
+    <!-- Utiliza valores del array data que le ha llegado desde routes/web.php -->
+    <h1>{{ $msj }} {{ $edad }}</h1>
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contacto', function () {
+    return "Contactame";
+})->name('contacto');
+/* ->name es un nombre que se le da a una ruta para que no tengamos que hacerlo de forma relativa */
+
+
+Route::get('/custom', function() {
+
+    $msj2 = "Mensaje desde el servidor *-*";
+
+    /*
+    Este ejemplo: 2º Parametro, envia para parametros a la vista custom, el nombre para utilizar en la vista es msj
+    return view('custom', ['msj' => $msj2, 'edad' => 15 ]);
+    */
+
+    $data = ['msj' => $msj2, 'edad' => 15 ];
+
+    //Envia un array de valores de $data
+    return view('custom', $data);
+});
